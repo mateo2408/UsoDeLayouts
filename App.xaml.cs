a63a67ee5cbcde54dcb85028bb4c4e89ce3ab96d
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-05-19 08:22:23 -0500

    Primer commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CisnerosM_UsodeLayouts;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
